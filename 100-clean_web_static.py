043e908a860480611302331510601d637afe99f9
Othino <micahothino44@gmail.com>
2023-01-23 17:07:51 -0800

    Fabric script (based on the file 3-deploy_web_static.py) that deletes out-of-date archives, using the function do_clean

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -7,6 +7,15 @@ env.hosts = ['<52.3.41.70 web-01>', '<52.86.37.174 web-02>']
 
 
 def do_clean(number=0):
+    """Delete all unnecessary archives (all archives minus the number to keep)
+    in the versions folder and the /data/web_static/releases folder of both of
+    your web servers
+    
+    Args:
+    number: int: number of the archives, including the most recent, to keep.
+    If number is 0 or 1, keep only the most recent version of your archive.
+    if number is 2, keep the most recent, and second most recent versions of your archive.
+    """
     if number == 0:
         number = 1
     archive_list = run("ls -1t /data/web_static/releases/").split()
