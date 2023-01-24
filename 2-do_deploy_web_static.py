fe30b334d2901c7a25ca31e46024cfb0a4c71e56
Othino <micahothino44@gmail.com>
2023-01-23 17:35:56 -0800

    Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -23,32 +23,32 @@ def do_deploy(archive_path):
 
                 # create target dir
                 timestamp = archive_path[-18:-4]
-                run('sudo mkdir -p /data/web_static/\
+                run('sudo mkdir -p /web_static/\
 releases/web_static_{}/'.format(timestamp))
 
                 # uncompress archive and delete .tgz
                 run('sudo tar -xzf /tmp/web_static_{}.tgz -C \
-/data/web_static/releases/web_static_{}/'
+/web_static/releases/web_static_{}/'
                     .format(timestamp, timestamp))
 
                 # remove archive
                 run('sudo rm /tmp/web_static_{}.tgz'.format(timestamp))
 
                 # move contents into host web_static
-                run('sudo mv /data/web_static/releases/web_static_{}/web_static/* \
-/data/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
+                run('sudo mv /web_static/releases/web_static_{}/web_static/* \
+/web_static/releases/web_static_{}/'.format(timestamp, timestamp))
 
                 # remove extraneous web_static dir
-                run('sudo rm -rf /data/web_static/releases/\
+                run('sudo rm -rf /web_static/releases/\
 web_static_{}/web_static'
                     .format(timestamp))
 
                 # delete pre-existing sym link
-                run('sudo rm -rf /data/web_static/current')
+                run('sudo rm -rf /web_static/current')
 
                 # re-establish symbolic link
-                run('sudo ln -s /data/web_static/releases/\
-web_static_{}/ /data/web_static/current'.format(timestamp))
+                run('sudo ln -s /web_static/releases/\
+web_static_{}/ /web_static/current'.format(timestamp))
         except:
                 return False
 
